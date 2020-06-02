a3254b9344d2b6270838b4833e230097a5ef540a
Reginer <282921012@qq.com>
2020-06-02 13:53:26 +0800

    NoNull

--- imcore/src/main/java/com/spd/im/core/listener/ILoginCallback.java
+++ imcore/src/main/java/com/spd/im/core/listener/ILoginCallback.java
@@ -1,6 +1,7 @@
 package com.spd.im.core.listener;
 
-import android.annotation.NonNull;
+
+import androidx.annotation.NonNull;
 
 /**
  * @author :Reginer on  2020/5/15 16:46.
@@ -8,7 +9,15 @@ import android.annotation.NonNull;
  * 功能描述:
  */
 public interface ILoginCallback {
+    /**
+     * 登录成功
+     */
     void onSuccess();
 
+    /**
+     * 登录失败
+     *
+     * @param e 错误信息
+     */
     void onFailed(@NonNull Exception e);
 }
